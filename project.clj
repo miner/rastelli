2e8057e890c1796832939274e680f906c26d91bb
Steve Miner <steveminer@gmail.com>
2020-01-10 10:26:13 -0500

    bump libs

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/miner/rastelli"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/test.check "0.10.0"]
                  #_ [org.clojure/data.int-map "0.2.4"]
                  ;; [ubergraph "0.4.1-sem"]
                  [aysylu/loom "1.0.2"]
-                 [quil "3.0.0"]])
+                 [quil "3.1.0"]])
