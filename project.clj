1cecebd7a7d1b2597f39cbfeaed330e0b7a1332d
Steve Miner <steveminer@gmail.com>
2020-10-03 13:57:05 -0400

    lib

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/test.check "1.0.0"]
+                 [org.clojure/test.check "1.1.0"]
                  #_ [org.clojure/data.int-map "0.2.4"]
                  ;; [ubergraph "0.4.1-sem"]
                  [aysylu/loom "1.0.2"]
