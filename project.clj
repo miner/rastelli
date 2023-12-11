15a8f219ddbdfbaff05bae379ed86d16c610fc85
Steve Miner <steveminer@gmail.com>
2023-12-11 15:45:56 -0500

    libs

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/miner/rastelli"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/test.check "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/test.check "1.1.1"]
                  #_ [org.clojure/data.int-map "0.2.4"]
                  ;; [ubergraph "0.4.1-sem"]
                  [aysylu/loom "1.0.2"]
-                 [quil "3.1.0"]])
+                 [quil "4.3.1323"]])
